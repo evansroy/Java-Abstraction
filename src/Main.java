c5fca8892ae2814aa74852fc248387035c9a6704
evansroy <evansroysir@gmail.com>
2023-03-05 18:21:28 +0300

    Added abstract class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+//Abstract class
+abstract class Animal{
+    //Abstract method (does not hava a body)
+    public abstract void animalSound();
+    //Regular Method
+    public void sleep(){
+        System.out.println("Zzzz");
+    }
+}
+
+//Subclass (inherit from animal)
+class Pig extends Animal {
+    public void animalSound(){
+        //The body of animalSound() is provided here
+        System.out.println("The pig says: Wee wee");
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pig myPig = new Pig();// create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
     }
 }
